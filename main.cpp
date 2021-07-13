dcbce5723848cff7ca02b43d8148a22ec49863a5
Alon Regev <alonr3704@gmail.com>
2021-07-13 14:37:08 +0300

    added cmd argument handler and basic commands

--- main.cpp
+++ main.cpp
@@ -3,45 +3,40 @@
 #include <string>
 #include <exception>
 
-void startMessage();
+#define VERSION "0.3"
+#define NAME "<nameless interpreter>"
+
 std::string codeInput();
+void runInterpreter();
 
-int main()
+int main(int argc, char** argv)
 {
-	startMessage();
-
-	Interpreter interpreter;
-	std::string input;
-	// code loop
-	do
+	if (argc == 1)
 	{
-		
-		input = codeInput();
-		// special input
-		if (input.empty() || input == "quit")
-			continue;
-		if (input == "clear")
-		{
-			system("cls");
-			continue;
-		}
-		// process input
-		try
-		{
-			std::string returnString = interpreter.run(input);
-			std::cout << returnString << std::endl;
-		}
-		catch(InterpreterException& e)
-		{
-			std::cout << e.what() << std::endl;
-		}
-	} while (input != "quit");
-}
+		runInterpreter();
+		return 0;
+	}
+	std::string command(argv[1]);
+	if (command == "--version")
+		std::cout << NAME << " " << VERSION << std::endl;
+	else if (command == "--help")
+		std::cout <<
+		"--help    : shows list of commands (this)\n"
+		"--version : shows version number\n\n"
 
-// fuction prints start message
-void startMessage()
-{
-	std::cout << "Welcome to <nameless interpreter> version 0.3!" << std::endl << std::endl;
+		"<file>    : runs a code file\n"
+		"-d        : runs file in line-by-line basic debug mode\n"
+		"-ad       : advanced debug mode\n";
+	else
+	{	
+		if (command.front() == '-')
+		{
+			std::cout << "invalid command. use --help to get list of commands." << std::endl;
+			return 1;
+		}
+		// run file
+		std::cout << "to do: run file " << command << std::endl;
+	}
 }
 
 // function gets code input, spanning multiple lines if there's ';' on the end (expecting another line)
@@ -61,4 +56,36 @@ std::string codeInput()
 		input += '\n' + temp;
 	}
 	return input;
+}
+
+// accepts code input from user and runs it
+void runInterpreter()
+{
+	std::cout << "Welcome to " << NAME << " version " << VERSION << "!" << std::endl << std::endl;
+
+	Interpreter interpreter;
+	std::string input;
+	// code loop
+	do
+	{
+		input = codeInput();
+		// special input
+		if (input.empty() || input == "quit")
+			continue;
+		if (input == "clear")
+		{
+			system("cls");
+			continue;
+		}
+		// process input
+		try
+		{
+			std::string returnString = interpreter.run(input);
+			std::cout << returnString << std::endl;
+		}
+		catch (InterpreterException& e)
+		{
+			std::cout << e.what() << std::endl;
+		}
+	} while (input != "quit");
 }
